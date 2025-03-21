a435d0e5c68860cc476bfcf3f2cea0496fd951e7
ha <ha>
2025-03-21 18:02:49 +0700

    finish

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -34,7 +34,7 @@ class AuthController extends Controller
             'isAdmin' => $isAdmin,
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => Auth::guard('api')->factory()->getTTL() * 60 
+            'expires_in' => Auth::guard('api')->factory()->getTTL()
         ]);
     }
 
